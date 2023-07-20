32ca46ddbc5e4a223004349d9f7b78a00314b9ee
Hayotunday <idowudanielayotunde@gmail.com>
2023-07-20 02:21:28 +0100

    updated cors options

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ import authRoutes from './routes/auth.js'
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' }));
+// Define a list of origins that you want to allow, in this case, just 'https://podcast-expert.vercel.app'
+const allowedOrigins = ['https://podcast-expert.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the request's origin is in the allowed list
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // This enables sending cookies in cross-origin requests
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.static('public'))
